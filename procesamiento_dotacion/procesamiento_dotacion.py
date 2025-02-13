b7ee1b9cbbf247bf040c567bcd51b63c0bacf422
Orozco Enzo Mariano <marianoorozco700@outlook.com>
2025-02-12 21:36:23 -0300

    Ultimo commit, lo juro

--- procesamiento_dotacion/procesamiento_dotacion.py
+++ procesamiento_dotacion/procesamiento_dotacion.py
@@ -3,65 +3,70 @@ import openpyxl
 from config import Config
 from datetime import datetime, timedelta
 from procesamiento_progra.preparacion_dnh import filtrado_de_columnas, obtencion_datos
+from tkinter import messagebox as mx
 
 def procesamiento_dotacion (fecha_input, dotacion, dimensionamiento):
         
-    # fecha_input = input("Ingrese la fecha del proximo sabado\n")
-    df_dimensionamiento = pd.read_excel(dimensionamiento)
-    df_dotacion = pd.read_excel(dotacion, usecols=["PROVEEDOR", "DNI", "NOMBRE Y APELLIDO Asesor" ,"LOG ID AVAYA", "SKILL", "PCRC", "LIDER", "HORARIO"])
+    try:   
+        # fecha_input = input("Ingrese la fecha del proximo sabado\n")
+        df_dimensionamiento = pd.read_excel(dimensionamiento)
+        df_dotacion = pd.read_excel(dotacion, usecols=["PROVEEDOR", "DNI", "NOMBRE Y APELLIDO Asesor" ,"LOG ID AVAYA", "SKILL", "PCRC", "LIDER", "HORARIO"])
 
 
-    ######## Colocacion de fecha para el filtrado de las columnas ##########
-    # convertimos ese dato en fecha
-    fecha = datetime.strptime(fecha_input,"%d-%m-%Y")
+        ######## Colocacion de fecha para el filtrado de las columnas ##########
+        # convertimos ese dato en fecha
+        fecha = datetime.strptime(fecha_input,"%d-%m-%Y")
 
-    # Creamos una lista vacia para poder almacenar las fechas que necesitamos
-    dias_semana = []
+        # Creamos una lista vacia para poder almacenar las fechas que necesitamos
+        dias_semana = []
 
-    # Almacenamos las fechas
-    dias_semana.append(fecha_input)
-    for i in range(1, 7):
-        dia = fecha + timedelta(days=i)
-        dia = dia.strftime("%d-%m-%Y")
-        dias_semana.append(dia)
+        # Almacenamos las fechas
+        dias_semana.append(fecha_input)
+        for i in range(1, 7):
+            dia = fecha + timedelta(days=i)
+            dia = dia.strftime("%d-%m-%Y")
+            dias_semana.append(dia)
+    except Exception as e:
+        mx.showerror("Error al convertir fecha", f"{e}")
 
-    # Dia sabado
-    columnas_filtradas = filtrado_de_columnas(df_dimensionamiento, dias_semana[0])
-    df_borrar = obtencion_datos(df_dimensionamiento, columnas_filtradas)
-    df_borrar.to_excel("probadno.xlsx", index=False)
+    try:
+        # Dia sabado
+        columnas_filtradas = filtrado_de_columnas(df_dimensionamiento, dias_semana[0])
+        df_borrar = obtencion_datos(df_dimensionamiento, columnas_filtradas)
 
-    df_borrar.to_excel("borrar.xlsx")
-    # Variable que nos cuenta la cantidad de asesores que tienen en su horario del DNH "vacaciones"
-    contador = 0
+        # Variable que nos cuenta la cantidad de asesores que tienen en su horario del DNH "vacaciones"
+        contador = 0
 
-    # Recorremos el dataframe df_borrar buscando los asesores que tengan en su horario "vacaciones"
-    for x in df_borrar.index:
-        if df_borrar.loc[x, "Horarios"] == "vacaciones":
-            avaya_borrar = df_borrar.loc[x, "Id Avaya"]
-            contador += 1
-            # filtrar la dotacion y que siempre que el id sea uno de estos. que modifique la columna horarios con "vacaciones"
-            for d in df_dotacion.index:
-                if df_dotacion.loc[d, "LOG ID AVAYA"] == avaya_borrar:
-                    df_dotacion.loc[d, "HORARIO"] = "vacaciones"
+        # Recorremos el dataframe df_borrar buscando los asesores que tengan en su horario "vacaciones"
+        for x in df_borrar.index:
+            if df_borrar.loc[x, "Horarios"] == "vacaciones":
+                avaya_borrar = df_borrar.loc[x, "Id Avaya"]
+                contador += 1
+                # filtrar la dotacion y que siempre que el id sea uno de estos. que modifique la columna horarios con "vacaciones"
+                for d in df_dotacion.index:
+                    if df_dotacion.loc[d, "LOG ID AVAYA"] == avaya_borrar:
+                        df_dotacion.loc[d, "HORARIO"] = "vacaciones"
+    except Exception as e:
+        mx.showerror("Procesamiento Sabado", f"Se produjo un error al intentar procesar la dotacion:\n {e}")
+    try:
+        # Dia Domingo
+        columnas_filtradas = filtrado_de_columnas(df_dimensionamiento, dias_semana[1])
+        df_borrar = obtencion_datos(df_dimensionamiento, columnas_filtradas)
+        df_borrar.to_excel("probadno.xlsx", index=False)
 
-
-    # Dia Domingo
-    columnas_filtradas = filtrado_de_columnas(df_dimensionamiento, dias_semana[1])
-    df_borrar = obtencion_datos(df_dimensionamiento, columnas_filtradas)
-    df_borrar.to_excel("probadno.xlsx", index=False)
-
-    # Recorremos el dataframe df_borrar buscando los asesores que tengan en su horario "vacaciones"
-    for x in df_borrar.index:
-        if df_borrar.loc[x, "Horarios"] == "vacaciones" or df_borrar.loc[x, "Horarios"] == " vacaciones" or df_borrar.loc[x, "Horarios"] == "vacaciones " :
-            avaya_borrar = df_borrar.loc[x, "Id Avaya"]
-            contador += 1
-            # filtrar la dotacion y que siempre que el id sea uno de estos. que modifique la columna horarios con "vacaciones"
-            for d in df_dotacion.index:
-                if df_dotacion.loc[d, "LOG ID AVAYA"] == avaya_borrar:
-                    df_dotacion.loc[d, "HORARIO"] = "vacaciones"
-            else:
-                print(f"Avaya DNH {avaya_borrar} - Avaya dotacion {df_dotacion.loc[d, "LOG ID AVAYA"]}")
-
+        # Recorremos el dataframe df_borrar buscando los asesores que tengan en su horario "vacaciones"
+        for x in df_borrar.index:
+            if df_borrar.loc[x, "Horarios"] == "vacaciones" or df_borrar.loc[x, "Horarios"] == " vacaciones" or df_borrar.loc[x, "Horarios"] == "vacaciones " :
+                avaya_borrar = df_borrar.loc[x, "Id Avaya"]
+                contador += 1
+                # filtrar la dotacion y que siempre que el id sea uno de estos. que modifique la columna horarios con "vacaciones"
+                for d in df_dotacion.index:
+                    if df_dotacion.loc[d, "LOG ID AVAYA"] == avaya_borrar:
+                        df_dotacion.loc[d, "HORARIO"] = "vacaciones"
+                else:
+                    print(f"Avaya DNH {avaya_borrar} - Avaya dotacion {df_dotacion.loc[d, "LOG ID AVAYA"]}")
+    except Exception as e:
+        mx.showerror("Procesamiento Domingo", f"Se produjo un error al intentar procesar la dotacion:\n {e} ")
 
     print("Cantidad de asesores de vacaciones: ",contador)
 
--- procesamiento_progra/preparacion_df_inicial.py
+++ procesamiento_progra/preparacion_df_inicial.py
@@ -1,35 +1,40 @@
 import pandas as pd
+from tkinter import messagebox as mx
 
 def repeticion_valores(data, dias_semana):
-    # Creamos un dataframe 
-    columns=["Fecha", "Lider", "PCRC","#Skill", "Plataforma", "ID", "DNI", "Nombre y Apellido","Horario",  "0:00", "0:30", "1:00", "1:30", "2:00", "2:30", "3:00", "3:30", "4:00", "4:30", "5:00", "5:30", "6:00", "6:30", "7:00", "7:30", "8:00", "8:30", "9:00", "9:30", "10:00", "10:30", "11:00", "11:30", "12:00", "12:30", "13:00", "13:30", "14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:00", "17:30", "18:00", "18:30", "19:00", "19:30", "20:00", "20:30", "21:00", "21:30", "22:00", "22:30","23:00", "23:30"]
-    df = pd.DataFrame(columns= columns)
-    data[0]
-    # Creamos un contador que nos permita indicar el indice donde queremos colocar el dato
-    contador = 0
+    try:
+        # Creamos un dataframe 
+        columns=["Fecha", "Lider", "PCRC","#Skill", "Plataforma", "ID", "DNI", "Nombre y Apellido","Horario",  "0:00", "0:30", "1:00", "1:30", "2:00", "2:30", "3:00", "3:30", "4:00", "4:30", "5:00", "5:30", "6:00", "6:30", "7:00", "7:30", "8:00", "8:30", "9:00", "9:30", "10:00", "10:30", "11:00", "11:30", "12:00", "12:30", "13:00", "13:30", "14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:00", "17:30", "18:00", "18:30", "19:00", "19:30", "20:00", "20:30", "21:00", "21:30", "22:00", "22:30","23:00", "23:30"]
+        df = pd.DataFrame(columns= columns)
+        data[0]
+        # Creamos un contador que nos permita indicar el indice donde queremos colocar el dato
+        contador = 0
 
-    # Colocamos los datos
-    for d in data:
-        for x in range(7):
-            contador = contador  + 1
-            df.loc[contador, "Fecha"] = dias_semana[x]
-            df.loc[contador, "Lider"] = d[6]
-            df.loc[contador, "PCRC"] = d[5]
-            df.loc[contador, "#Skill"] = d[4]
-            df.loc[contador, "ID"] = d[3]
-            df.loc[contador, "Nombre y Apellido"] = d[2]
-            df.loc[contador, "DNI"] = d[1]
-            df.loc[contador, "Horario"] = d[7]
+        # Colocamos los datos
+        for d in data:
+            for x in range(7):
+                contador = contador  + 1
+                df.loc[contador, "Fecha"] = dias_semana[x]
+                df.loc[contador, "Lider"] = d[6]
+                df.loc[contador, "PCRC"] = d[5]
+                df.loc[contador, "#Skill"] = d[4]
+                df.loc[contador, "ID"] = d[3]
+                df.loc[contador, "Nombre y Apellido"] = d[2].replace(",", "")
+                df.loc[contador, "DNI"] = d[1]
+                df.loc[contador, "Horario"] = d[7]
 
-    df["Plataforma"] = "GSS ARG"
-    # Cambiamos el formato de los pcrc 
-    for i in range(1, len(df)):
-        if df.loc[i, "PCRC"] == "1L convergente":
-            df.loc[i, "PCRC"] = "1L CONVERGENTE COM"
-        elif df.loc[i, "PCRC"] == "1L Convergente":
-            df.loc[i, "PCRC"] = "1L CONVERGENTE COM"
-        elif df.loc[i, "PCRC"] == "1L convergente cross":
-            df.loc[i, "PCRC"] = "1L CONVERGENTE CROSSELLING"
+        df["Plataforma"] = "GSS ARG"
+        # Cambiamos el formato de los pcrc 
+        for i in range(1, len(df)):
+            if df.loc[i, "PCRC"] == "1L convergente":
+                df.loc[i, "PCRC"] = "1L CONVERGENTE COM"
+            elif df.loc[i, "PCRC"] == "1L Convergente":
+                df.loc[i, "PCRC"] = "1L CONVERGENTE COM"
+            elif df.loc[i, "PCRC"] == "1L convergente cross":
+                df.loc[i, "PCRC"] = "1L CONVERGENTE CROSSELLING"
 
+        df["Nombre y Apellido"] = df["Nombre y Apellido"].replace(",", "")
+    except Exception as e:
+        mx.showerror("Repeticion valores", f"Se produjo un error en la funcion que se encarga de coloca los valores iniciales en el df principal. Error: {e}")
     # Devolvemos el dataframe
     return df
